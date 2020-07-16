295e0b2be1032f0932e0bc455603741dc8fb95dd
Winton DeShong <contact@winton.me>
2020-07-16 16:05:21 -0400

    Added tests for atoms submit button to fix issue #8

--- src/atoms/forms/submit-button.test.tsx
+++ src/atoms/forms/submit-button.test.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import { SubmitButton } from "./submit-button";
+import faker from "faker";
+import { ButtonStyles } from "../constants/button-styles";
 
 describe("SubmitButton", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React.Components/issues/8", () => {});
+    test("when default props, renders default buttonText", async () => {
+        // Arrange
+        const expected = "Submit";
+
+        // Act
+        const { getByText } = render(<SubmitButton />);
+
+        // Assert
+        expect(getByText(expected)).not.toBeNull();
+    });
+    test("when buttonText provided, renders supplied buttonText", async () => {
+        // Arrange
+        const expected = faker.random.words();
+
+        // Act
+        const { getByText } = render(<SubmitButton buttonText={expected} />);
+
+        // Assert
+        expect(getByText(expected)).not.toBeNull();
+    });
+
+    test("when cssClassName provided, renders with className set", async () => {
+        // Arrange
+        const expected = ButtonStyles.Anchor;
+
+        // Act
+        const { container } = render(<SubmitButton cssClassName={expected} />);
+
+        // Assert
+        expect(container.firstChild.className).toContain(expected);
+    });
 });
