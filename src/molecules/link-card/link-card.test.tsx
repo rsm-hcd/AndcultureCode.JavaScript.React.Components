6513f3cb5c5b70bc4a2e10d533e3e4ec87bb1fdf
Said Shah <sshah@andculture.com>
2020-10-15 12:43:43 -0400

    Added more tests for increased test coverage

--- src/molecules/link-card/link-card.test.tsx
+++ src/molecules/link-card/link-card.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import faker from "faker";
 import { LinkCard } from "./link-card";
 import { TestUtils } from "../../tests/test-utils";
+import { LinkCardTypes } from "../constants/link-card-types";
 
 describe("LinkCard", () => {
     test("When default props, renders LinkCard", () => {
@@ -16,5 +17,43 @@ describe("LinkCard", () => {
 
         //Assert
         expect(getByText(expected)).not.toBeNull();
+    });
+
+    test("When type prop is button, renders LinkCard with button", () => {
+        // Arrange
+        const expected = faker.random.words();
+        const label = faker.random.words();
+        const type = LinkCardTypes.Button;
+
+        // Act
+        const { container } = TestUtils.renderWithRouter(
+            <LinkCard label={label} type={type}>
+                {expected}
+            </LinkCard>
+        );
+        const result = container.getElementsByTagName(LinkCardTypes.Button)[0];
+
+        // Assert
+        expect(result).not.toBeNull();
+        expect(result.classList).toContain("c-button");
+        expect(container.innerHTML).toContain("button");
+    });
+
+    test("When default props and include icon, renders with class name -with-icon", () => {
+        // Arrange
+        const expected = faker.random.words();
+        const label = faker.random.words();
+
+        // Act
+        const { container } = TestUtils.renderWithRouter(
+            <LinkCard label={label} includeIcon={true}>
+                {expected}
+            </LinkCard>
+        );
+        const result = container.getElementsByClassName("-with-icon")[0];
+
+        // Assert
+        expect(result).not.toBeNull();
+        expect(result.classList).toContain("-with-icon");
     });
 });
