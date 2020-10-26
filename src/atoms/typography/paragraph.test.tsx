964dade4cfdf503ce751836a04d862b534d8eef6
Said Shah <sshah@andculture.com>
2020-10-26 13:49:53 -0400

    Added tests for paragraphs

--- src/atoms/typography/paragraph.test.tsx
+++ src/atoms/typography/paragraph.test.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import faker from "faker";
+import { Paragraph } from "./paragraph";
 
 describe("Paragraph", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React.Components/issues/15", () => {});
+    test("when default props, renders paragraph with text", () => {
+        // Act
+        const expected = faker.random.words();
+
+        // Arrange
+        const { getByText } = render(<Paragraph>{expected}</Paragraph>);
+
+        // Assert
+        expect(getByText(expected)).not.toBeNil();
+    });
+
+    test("when given cssClassName prop, renders paragraph with given class name", () => {
+        // Act
+        const expected = faker.random.words();
+        const testClassName = faker.random.word();
+
+        // Arrange
+        const { container } = render(
+            <Paragraph cssClassName={testClassName}>{expected}</Paragraph>
+        );
+        const result = container.querySelector("." + testClassName);
+
+        // Assert
+        expect(result).not.toBeNil();
+    });
 });
