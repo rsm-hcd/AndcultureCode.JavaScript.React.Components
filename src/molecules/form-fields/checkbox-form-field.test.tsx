cebc50f5fff3528ac9487f556eecfc0054816407
Said Shah <sshah@andculture.com>
2020-10-29 11:39:51 -0400

    Added tests for checkbox form field

--- src/molecules/form-fields/checkbox-form-field.test.tsx
+++ src/molecules/form-fields/checkbox-form-field.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { CheckboxFormField } from "./checkbox-form-field";
 import faker from "faker";
 
@@ -19,5 +19,45 @@ describe("CheckboxFormField", () => {
 
         // Assert
         expect(getByLabelText(expected)).not.toBeNull();
+    });
+
+    test("when errorsMessage prop set, renders with error message", () => {
+        // Arrange
+        const expected = faker.random.words();
+        const testErrorMessage = "testErrorMessage";
+
+        // Act
+        const { getByText } = render(
+            <CheckboxFormField
+                checked={false}
+                label={expected}
+                onChange={() => {}}
+                errorMessage={testErrorMessage}
+            />
+        );
+
+        // Assert
+        expect(getByText(testErrorMessage)).not.toBeNil();
+    });
+
+    test("when onChange set, calls handler upon change", () => {
+        // Arrange
+        let isChecked = false;
+        const expected = faker.random.word();
+        const handleChange = () => (isChecked = true);
+
+        // Act
+        const { getByText } = render(
+            <CheckboxFormField
+                checked={isChecked}
+                label={expected}
+                onChange={handleChange}
+            />
+        );
+
+        fireEvent.click(getByText(expected));
+
+        // Assert
+        expect(isChecked).toBeTrue();
     });
 });
