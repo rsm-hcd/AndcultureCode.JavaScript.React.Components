d75e17a646ace6078e0f870f7840c4b8928cabed
Said Shah <sshah@andculture.com>
2020-11-02 10:52:34 -0500

    Added tests for heading icon

--- src/atoms/typography/heading-icon.test.tsx
+++ src/atoms/typography/heading-icon.test.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import faker from "faker";
+import { HeadingIcon } from "./heading-icon";
+import { render } from "@testing-library/react";
+import { Icons } from "../constants/icons";
 
 describe("HeadingIcon", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React.Components/issues/14", () => {});
+    test("when default props, renders Heading with Icon", () => {
+        // Arrange
+        const expected = faker.random.words();
+
+        // Act
+        const { getByText } = render(
+            <HeadingIcon type={Icons.Checkmark}>{expected}</HeadingIcon>
+        );
+
+        // Assert
+        expect(getByText(expected)).not.toBeNil();
+    });
 });
