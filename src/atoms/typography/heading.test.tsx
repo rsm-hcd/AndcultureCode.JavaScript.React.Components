bd4d74453f39c3ca41f1bfd668286166381413ea
Said Shah <sshah@andculture.com>
2020-11-02 10:02:22 -0500

    Added tests for heading

--- src/atoms/typography/heading.test.tsx
+++ src/atoms/typography/heading.test.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import faker from "faker";
+import { Heading } from "./heading";
+import { render } from "@testing-library/react";
 
 describe("Heading", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React.Components/issues/14", () => {});
+    test("when default props, renders heading", () => {
+        // Assert
+        const expected = faker.random.words();
+
+        // Act
+        const { getByText } = render(<Heading>{expected}</Heading>);
+
+        // Assert
+        expect(getByText(expected)).not.toBeNil();
+    });
+
+    test("when given cssClassName prop, renders with given class name", () => {
+        // Act
+        const randomText = faker.random.words();
+        const testClassName = "testClassName";
+
+        // Arrange
+        const { container } = render(
+            <Heading cssClassName={testClassName}>{randomText}</Heading>
+        );
+        const result = container.getElementsByClassName(testClassName);
+
+        // Assert
+        expect(result[0]).not.toBeNil();
+    });
 });
