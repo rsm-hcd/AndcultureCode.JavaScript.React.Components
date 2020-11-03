8ed51b8036993181861e0196ec4e7016b1933e77
Said Shah <sshah@andculture.com>
2020-11-03 11:03:31 -0500

    Added tests for toast manager

--- src/utilities/toast-manager.test.ts
+++ src/utilities/toast-manager.test.ts
@@ -1,3 +1,48 @@
+import faker from "faker";
+import { ToastManager } from "./toast-manager";
+
 describe("ToastManager", () => {
-    test.skip("TODO: Backfill tests for issue https://github.com/AndcultureCode/AndcultureCode.JavaScript.React.Components/issues/34", () => {});
+    test("when error toast created, returns toastId", () => {
+        // Arrange & Act
+        const testToastId = faker.random.number(99);
+        const toastContent = faker.random.word();
+        const toastOptions = { toastId: testToastId };
+        const toastId = ToastManager.error(toastContent, toastOptions);
+
+        // Act
+        expect(toastId).toBe(testToastId);
+    });
+
+    test("when info toast created, returns toastId", () => {
+        // Arrange & Act
+        const testToastId = faker.random.number(99);
+        const toastContent = faker.random.word();
+        const toastOptions = { toastId: testToastId };
+        const toastId = ToastManager.info(toastContent, toastOptions);
+
+        // Act
+        expect(toastId).toBe(testToastId);
+    });
+
+    test("when success toast created, returns toastId", () => {
+        // Arrange & Act
+        const testToastId = faker.random.number(99);
+        const toastContent = faker.random.word();
+        const toastOptions = { toastId: testToastId };
+        const toastId = ToastManager.success(toastContent, toastOptions);
+
+        // Act
+        expect(toastId).toBe(testToastId);
+    });
+
+    test("when warn toast created, returns toastId", () => {
+        // Arrange & Act
+        const testToastId = faker.random.number(99);
+        const toastContent = faker.random.word();
+        const toastOptions = { toastId: testToastId };
+        const toastId = ToastManager.warn(toastContent, toastOptions);
+
+        // Act
+        expect(toastId).toBe(testToastId);
+    });
 });
