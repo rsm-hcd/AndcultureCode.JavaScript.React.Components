13a9412e7176689b2a37b6bdcd1071785484109d
Said Shah <sshah@andculture.com>
2020-11-03 11:46:59 -0500

    Updated tests for toast manager

--- src/utilities/toast-manager.test.ts
+++ src/utilities/toast-manager.test.ts
@@ -3,46 +3,90 @@ import { ToastManager } from "./toast-manager";
 
 describe("ToastManager", () => {
     test("when error toast created, returns toastId", () => {
-        // Arrange & Act
+        // Arrange
         const testToastId = faker.random.number(99);
         const toastContent = faker.random.word();
         const toastOptions = { toastId: testToastId };
-        const toastId = ToastManager.error(toastContent, toastOptions);
 
         // Act
+        const toastId = ToastManager.error(toastContent, toastOptions);
+
+        // Assert
         expect(toastId).toBe(testToastId);
     });
 
     test("when info toast created, returns toastId", () => {
-        // Arrange & Act
+        // Arrange
         const testToastId = faker.random.number(99);
         const toastContent = faker.random.word();
         const toastOptions = { toastId: testToastId };
-        const toastId = ToastManager.info(toastContent, toastOptions);
 
         // Act
+        const toastId = ToastManager.info(toastContent, toastOptions);
+
+        // Assert
         expect(toastId).toBe(testToastId);
     });
 
     test("when success toast created, returns toastId", () => {
-        // Arrange & Act
+        // Arrange
         const testToastId = faker.random.number(99);
         const toastContent = faker.random.word();
         const toastOptions = { toastId: testToastId };
-        const toastId = ToastManager.success(toastContent, toastOptions);
 
         // Act
+        const toastId = ToastManager.success(toastContent, toastOptions);
+
+        // Assert
         expect(toastId).toBe(testToastId);
     });
 
     test("when warn toast created, returns toastId", () => {
-        // Arrange & Act
+        // Arrange
         const testToastId = faker.random.number(99);
         const toastContent = faker.random.word();
         const toastOptions = { toastId: testToastId };
-        const toastId = ToastManager.warn(toastContent, toastOptions);
 
         // Act
+        const toastId = ToastManager.warn(toastContent, toastOptions);
+
+        // Assert
         expect(toastId).toBe(testToastId);
+    });
+
+    test("when toast dismissed, calls dismiss method", () => {
+        // Arrange
+        const testId = faker.random.number(99);
+        const dismissSpy = jest.spyOn(ToastManager, "dismiss");
+
+        // Act
+        ToastManager.dismiss(testId);
+
+        // Assert
+        expect(dismissSpy).toHaveBeenCalled();
+    });
+
+    test("when dismiss All toasts, calls dismissAll method", () => {
+        // Arrange
+        const dismissSpy = jest.spyOn(ToastManager, "dismissAll");
+
+        // Act
+        ToastManager.dismissAll();
+
+        // Assert
+        expect(dismissSpy).toHaveBeenCalled();
+    });
+
+    test("when update toast, calls update method", () => {
+        // Arrange
+        const dismissSpy = jest.spyOn(ToastManager, "update");
+        const newContent = faker.random.words();
+        const testId = faker.random.number(99);
+
+        // Act
+        ToastManager.update(testId, newContent);
+
+        // Assert
+        expect(dismissSpy).toHaveBeenCalled();
     });
 });
