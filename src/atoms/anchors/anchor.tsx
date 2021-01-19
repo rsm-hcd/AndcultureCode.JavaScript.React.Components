9adefd8cb7b2e273b0b48ed4c6f9487f2db0c324
Alex Getty <alexjgetty@gmail.com>
2021-01-19 11:07:23 -0700

    Moved anchor styles to component folder

--- src/atoms/anchors/anchor.tsx
+++ src/atoms/anchors/anchor.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { forwardRef } from "react";
+import "./anchor.scss";
 
 /*
 -----------------------------------------------------------------------------------------
